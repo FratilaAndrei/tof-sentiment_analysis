f0f3030895295aa345845ea58ce0ea226a79c5a0
Andrei Fratila <andrei@Andreis-MacBook-Air.local>
2023-07-25 01:06:44 +0300

    Added responsive from tablet to last on Raiders page

--- client/src/pages/Riders/RiderDataPanel.tsx
+++ client/src/pages/Riders/RiderDataPanel.tsx
@@ -1,17 +1,16 @@
 import React from "react";
 import JonasV from "../../utils/Jonas Vingegaard.jpg";
 import TadejP from "../../utils/Tadej.png";
-import CarlosR from "../../utils/CarlosR.png";
 import AdamY from "../../utils/AdamY.png";
-import JaiH from "../../utils/JaiH.png";
+import JaiH from "../../utils/jaiHindley-removebg-preview.png";
+import CarlosR from "../../utils/carlosRodriguez-removebg-preview.png";
 
 const RiderDataPanel = () => {
   const riderData = [
     {
       image: JonasV,
       name: "Jonas Vingegaard",
-      description:
-        "Danish cyclist who competes for Team Jumbo–Visma, a UCI WorldTeam licensed team",
+      description: "Danish cyclist who competes for Team Jumbo–Visma",
       birthDate: "December 10, 1996",
       weight: "60 KG",
       height: "1.75 m",
@@ -36,8 +35,7 @@ const RiderDataPanel = () => {
     {
       image: AdamY,
       name: " Adam Yates",
-      description:
-        "British road and track cyclist and the twin brother of Simon Yates who rides for the Orica-GreenEDGE team",
+      description: "British road and track cyclist for UAE Team Emirates",
       birthDate: "August 7, 1992",
       weight: "58 KG",
       height: "1.73 m",
@@ -46,7 +44,7 @@ const RiderDataPanel = () => {
       image: JaiH,
       name: "Jai Hindley",
       description:
-        "Australian professional cyclist who rides for UCI WorldTeam Bora–Hansgrohe. He has been successful in the Giro d'Italia",
+        "Australian cyclist who rides for UCI WorldTeam Bora–Hansgrohe",
       birthDate: "May 5, 1996",
       weight: "60 KG",
       height: "1.75 m",
@@ -55,9 +53,12 @@ const RiderDataPanel = () => {
 
   const getRaiderDataPanel = () => {
     return riderData.map((rider, index) => (
-      <div className="flex h-[450px] w-1/3 justify-center " key={index}>
+      <div
+        className="flex h-[350px] lg:h-[375px] 2xl:h-[500px] 2xl:mt-20 2xl:w-1/5 w-1/3 lg:px-2 xl:w-1/4  justify-center "
+        key={index}
+      >
         <div className="bg-gradient-to-br from-orange-500 to-yellow-500 flex rounded-lg w-[95%] h-[95%]">
-          <div className="w-1/3 h-auto px-2 flex py-20 ">
+          <div className="w-1/3 h-[350px] 2xl:h-[450px] px-2 flex py-20 ">
             <img
               src={rider.image}
               alt=""
@@ -65,10 +66,10 @@ const RiderDataPanel = () => {
             />
           </div>
           <div className="w-2/3 px-4 flex flex-col justify-center ">
-            <div className="pb-4 text-2xl pt-4 text-center text-white h-1/4 flex items-center ">
+            <div className="pb-4 text-md lg:text-xl 2xl:text-3xl pt-4 text-center text-white h-1/4 flex items-center ">
               {rider.name}
             </div>
-            <div className="text-white flex flex-col text-xl h-3/4 gap-2 ">
+            <div className="text-white flex flex-col text-md lg:text-lg 2xl:text-3xl h-3/4 gap-2 2xl:gap-6 ">
               <div>{rider.description}</div>
               <div>{rider.birthDate}</div>
               <div>{rider.weight}</div>
